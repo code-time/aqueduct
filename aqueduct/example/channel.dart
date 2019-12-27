5baf18f8709f88cfa0fbd7e4afc1a926b3b2b7b8
Joao V <36869425+jvworks@users.noreply.github.com>
2019-12-26 20:13:54 -0400

    examples

---
+++ aqueduct/example/channel.dart
@@ -0,0 +1,23 @@
+import 'package:aqueduct/aqueduct.dart';
+
+
+class SmartcoinChannel extends ApplicationChannel {
+
+  @override
+  Future prepare() async {
+    logger.onRecord.listen((rec) => print("$rec ${rec.error ?? ""} ${rec.stackTrace ?? ""}"));
+  }
+
+  @override
+  Controller get entryPoint {
+    final router = Router();
+
+    router
+      .route("/example")
+      .linkFunction((request) async {
+        return Response.ok({"key": "value"});
+      });
+
+    return router;
+  }
+}
